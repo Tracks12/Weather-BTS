58fa050ea9c09bdf16305118af6c40ddea33f307
Anarchy <anon.dark97@gmail.com>
2018-09-29 22:47:22 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,23 +6,30 @@
 *                              *
 *******************************/
 
-var menu = Array('home', 'live', 'story', 'contact');
+var menu = Array('home', 'live', 'story', 'contact', 'login'),
+		title = Array('Accueil', 'Temps Réel', 'Historique', 'Contact', 'Connexion');
 
 function mainMenu() {
-	var x, cell = document.getElementsByClassName('menu');
+	var x, y, cell = document.getElementsByClassName('menu'),
+			data = document.location.search.split('?')[1];
+	if(data) { data = data.split('=')[1]; }
 	
-	switch(document.location.search.split('?')[1].split('=')[1]) {
+	switch(data) {
+		case 'live': x = 1, y = 1; break;
+		case 'story': x = 2, y = 2; break;
+		case 'contact': x = 3, y = 3; break;
+		case 'login': x = 0, y = 4; break;
 		case 'home':
-		default: x = 0; break;
-		case 'live': x = 1; break;
-		case 'story': x = 2; break;
-		case 'contact': x = 3; break;
+		default: x = 0; y = 0; break;
 	}
 	
 	cell[x].style.color = '#00DDDD';
 	cell[x].style.paddingLeft = '25px';
 	cell[x].style.textShadow = '1px 1px 2px #00DDDD';
 	cell[x].style.width = '82.6%';
+	
+	document.title += " - "+title[y];
+	document.getElementsByTagName('h1')[0].innerHTML += " - "+title[y];
 }
 
 /******
