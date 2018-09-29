6e56711f725bfac56dfb202712d833b60f428242
Anarchy <anon.dark97@gmail.com>
2018-09-29 22:15:20 +0200

    Update header.php

--- assets/header.php
+++ assets/header.php
@@ -1,5 +1,5 @@
 <header>
 	<?php if(!isset($thead)) { $thead[1] = ''; } ?>
-	<h1>Météo BTS SN-EC<?php echo($thead[1]); ?></h1>
+	<h1>Météo BTS SN-EC<?php echo(" - ".$thead[1]); ?></h1>
 </header>
 <!-- END -->
