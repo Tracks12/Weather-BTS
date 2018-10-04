f89ca304196ee31f430a222a69f91c0f4069ca12
Anarchy <anon.dark97@gmail.com>
2018-10-05 00:16:41 +0200

    Add files via upload

--- assets/story.php
+++ assets/story.php
@@ -1,3 +1,29 @@
-<?php
-	
-?>
+<!DOCTYPE html>
+<!-- HISTORIQUE -->
+<div class="carousel">
+	<article>
+		<?php
+			$data = './data.csv';
+			$handle = fopen($data, 'r');
+			for($x = 0; $ligne = fgetcsv($handle, 1000, ';'); $x++) { } // On voit combien il y a de ligne
+			fclose($handle);
+			
+			$x = $x - 20; // On soustrait le nombre de ligne à sortir
+			$handle = fopen($data, 'r');
+			
+			echo("<table>");
+			for($i = 0; $ligne = fgetcsv($handle, 1000, ';'); $i++) {
+				if(($i >= $x) || !$i) { // On fait une condition pour afficher le nb de ligne souhaité
+					$cell = 'td'; if(!$i) { $cell = 'th'; }
+					echo("<tr>");
+					for($j = 0; $j < count($ligne); $j++) {
+						if($ligne[$j] || !$j) { echo("<$cell>$ligne[$j]</$cell>"); }
+					} echo("</tr>");
+				}
+			} echo("</table>");
+			
+			fclose($handle);
+		?>
+	</article>
+</div>
+<!-- END -->
