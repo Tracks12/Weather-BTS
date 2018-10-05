300c3da3a45af8e9e2199e4083045f8f527ab090
Anarchy <anon.dark97@gmail.com>
2018-10-05 22:10:29 +0200

    Add files via upload

--- assets/story.php
+++ assets/story.php
@@ -2,28 +2,37 @@
 <!-- HISTORIQUE -->
 <div class="carousel">
 	<article>
-		<?php
-			$data = './data.csv';
-			$handle = fopen($data, 'r');
-			for($x = 0; $ligne = fgetcsv($handle, 1000, ';'); $x++) { } // On voit combien il y a de ligne
-			fclose($handle);
-			
-			$x = $x - 20; // On soustrait le nombre de ligne à sortir
-			$handle = fopen($data, 'r');
-			
-			echo("<table>");
-			for($i = 0; $ligne = fgetcsv($handle, 1000, ';'); $i++) {
-				if(($i >= $x) || !$i) { // On fait une condition pour afficher le nb de ligne souhaité
+		<table>
+			<?php
+				$data = './donnees.csv';
+				$handle = fopen($data, 'r');
+				for($x = 0; $ligne = fgetcsv($handle, 1000, ';'); $x++) { // On voit combien il y a de ligne
+					for($y = 0; $y < count($ligne); $y++) { $output[$x][$y] = $ligne[$y]; } // On stock les valeur dans une variable tampon
+				} fclose($handle);
+				
+				for($i = 0; $i < count($output); $i++) { // On sort la ligne de la variable tampon
 					$cell = 'td'; if(!$i) { $cell = 'th'; }
 					echo("<tr>");
-					for($j = 0; $j < count($ligne); $j++) {
-						if($ligne[$j] || !$j) { echo("<$cell>$ligne[$j]</$cell>"); }
+					foreach($output[$i] as $col) { // On en sort ses colonnes
+						echo("<$cell>$col</$cell>");
 					} echo("</tr>");
+					if(!$i) { $i = $x-20; } // On paramètre le nombre de ligne attendu à la sortie
 				}
-			} echo("</table>");
-			
-			fclose($handle);
-		?>
+			?>
+		</table>
+	</article>
+	<article id="humidity">
+		<table>
+			<?php
+				
+			?>
+		</table>
+	</article>
+	<article id="temp">
+	</article>
+	<article id="pressure">
+	</article>
+	<article id="carbon">
 	</article>
 </div>
 <!-- END -->
