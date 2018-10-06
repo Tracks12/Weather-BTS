418dffe687d079efa6b93f6d88538eac06c161fd
Anarchy <anon.dark97@gmail.com>
2018-10-06 14:01:42 +0200

    Add files via upload

--- assets/nav.php
+++ assets/nav.php
@@ -7,7 +7,7 @@
 			if(isset($_SESSION['user'])) {
 				echo('<li><a class="menu" href="?index=home">Accueil</a></li>
 					<li><a class="menu" href="?index=live">Live</a></li>
-					<li><a class="menu" href="?index=story#main" id="path">Historique ></a></li>
+					<li><a class="menu" href="?index=story#main">Historique ></a></li>
 					<li><a class="menu" href="?index=contact">Contact</a></li>
 					<li><a class="menu" href="?logout">Déconnexion</a></li>');
 			} else { echo('<li><a class="menu" href="?index=login">Connexion</a></li>'); }
@@ -15,7 +15,7 @@
 	</ul>
 	<?php
 		if(isset($_SESSION['user'])) {
-			echo('<ol id="subMenu" hidden>
+			echo('<ol hidden>
 				<li style="border-top: 0;"><a href="./?index=story#humidity">Taux humidité</a></li>
 				<li><a href="./?index=story#temp">Temperature</a></li>
 				<li><a href="./?index=story#pressure">Pression</a></li>
